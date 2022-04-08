1bdaa67cc289639b7e11ef3399ce3960bd4f0355
Ubuntu <ubuntu@ip-172-31-34-72.ap-south-1.compute.internal>
2022-04-08 04:51:37 +0000

    first commit

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '1063777451895-osoi3ce5126b3hb7me4mhp7cvdbm5ei5.apps.googleusercontent.com',
+            clientSecret: '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'
+        },
+        mongodb: {
+            dbURI: 'mongodb://iamshaunjp:test@ds151024.mlab.com:51024/oauth-test'
+        }
+};
