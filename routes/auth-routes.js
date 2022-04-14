4ca46d8c1a06517fdac594882f8d73945557920d
lexacode <lexacode@lexacodes-MacBook-Air.local>
2022-04-14 07:17:51 +0500

    google authenticator removed

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -27,7 +27,7 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+router.get('/google/redirect', (req, res) => {
     res.send('you reached the redirect URI');
 });
 
